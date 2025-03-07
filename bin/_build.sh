9f53aa55d3dfc597f16bb109678942b819c80802
John Pritchard <985656+JohnPritchard@users.noreply.github.com>
2025-03-07 17:44:57 +0100

    [WIP]

--- bin/_build.sh
+++ bin/_build.sh
@@ -450,7 +450,7 @@ c_dependency () {
 
   export              PATH="${dstroot}/bin:${PATH}";
   export    C_INCLUDE_PATH="${dstroot}/include:${C_INCLUDE_PATH:-}";
-  export            CFLAGS="${CFLAGS:-}";
+  export            CFLAGS="${CFLAGS}";
   export   LD_LIBRARY_PATH="${dstroot}/lib:${dstroot}/lib64:${LD_LIBRARY_PATH:-}";
   export          CPPFLAGS="-I${dstroot}/include ${CPPFLAGS:-} ";
   export           LDFLAGS="-L${dstroot}/lib -L${dstroot}/lib64 ${LDFLAGS:-} ";
